f23d9933edd3e0c14b50a15c79cc7b23d141c6c5
Livestronger08 <derek.downie@ttu.edu>
2020-01-20 04:05:36 +0000

    Lesson 12 and Lesson 14 are fixed due to error with will_pagination from Lesson 12

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,7 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.order(:id).paginate(page:params[:page], per_page: 10)
   end
 
   def new
@@ -18,6 +18,5 @@ class PlacesController < ApplicationController
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 
 end
