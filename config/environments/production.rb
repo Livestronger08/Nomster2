1d1089b3c23c77c6f761a0108a5dafbd060b4fce
Livestronger08 <derek.downie@ttu.edu>
2020-01-29 19:36:02 +0000

    Lesson 44 production.rb fixed due to url error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: 'nomster-brolin.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'nomster-brolin2.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
